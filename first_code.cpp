138301e0116ea318b76e2062338c51248aaec87a
puneetcode31 <puneetasp31@gmail.com>
2024-11-30 12:57:04 +0530

    first code

---
+++ first_code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    x=7;
+    y=7;
+    cout<<++x;
+    cout<<endl;
+    cout<<y++;
+    cout<<endl;
+    cout<<x;
+    return 0;
+}
---
+++ learning code/code.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello world";
+    cout<<"\n ram ram ";
+    return 0;
+
+}
