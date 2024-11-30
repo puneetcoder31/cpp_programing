f803ad25f78a40adcf879b582cf98a89ec43dae6
puneetcode31 <puneetasp31@gmail.com>
2024-11-30 16:08:50 +0530

    third code

---
+++ evenOdd.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter the number =";
+    cin>>x;
+    if (x%2==0){
+        cout<<"given number is even =";
+        cout<<x;
+    }    
+    else {
+        cout<<"given number is odd =";
+        cout<<x;
+
+    }
+}
