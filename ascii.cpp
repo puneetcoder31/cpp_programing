6175271cc96adc12758d296fe8af5f9e8d1538f6
puneetcoder31 <puneetasp31@gmail.com>
2024-12-02 18:57:00 +0530

    to find ascii of character

---
+++ ascii.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    char x='a';
+    cout<<"ASCII   |  character"<<endl;
+    while(x<='z'){
+    cout<<(int)x<<"   =  "<<x<<endl;
+       x++;}
+    }
