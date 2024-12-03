4297f407f477d4728a004c166fe4829df9b43d2e
puneetcoder31 <puneetasp31@gmail.com>
2024-12-03 15:22:09 +0530

    sum of n number through recursion

---
+++ sum_recursion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int sum(int n){
+    if (n==1)  return 1 ;
+    return n +  sum(n-1);
+    
+    }
+int main()
+   {int x ;
+    cout<<"enter value of n=";
+    cin>>x;
+    cout<<sum(x);
+
+}
