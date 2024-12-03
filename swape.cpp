418902e5ef63963880c8d2b8dd83de943417a018
puneetcoder31 <puneetasp31@gmail.com>
2024-12-03 12:38:51 +0530

    to swape the values using third varibale

---
+++ swape.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int x,y,z;
+    cout<<"enter the  first number =";
+    cin>>x;
+    cout<<"enter the second number =";
+    cin>>y;
+    z=x;
+    x=y;
+    y=z;
+    cout<<"after  swape are first number= "<<x<<" and second number ="<<y;}
