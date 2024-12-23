ef3be7c6ef093c1e52ac1ebb49a316c243b01f19
puneetcoder31 <puneetasp31@gmail.com>
2024-12-23 23:17:00 +0530

    to find size of string

---
+++ sizeofstring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+     
+string nums[] = {"puneet"};
+cout<<sizeof(nums)<<endl;
+cout << sizeof(nums) / sizeof(nums[0]);}
+
+
