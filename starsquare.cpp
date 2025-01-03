8e9bf2c8b73e31232c60c8b0b99749ec89260a35
puneetcoder31 <puneetasp31@gmail.com>
2025-01-03 18:32:03 +0530

    to print pattern

---
+++ starsquare.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){ int a;
+    cout<<("enter the number of rows you want=");
+    cin>>a;
+    int c;
+        cout<<("enter the number of column you want=");
+        cin>>c;
+    for(int x=0;x<=a;x++){
+        for(int b=0;b<=c;b++ ){
+        cout<<(" * ");}
+        cout<<endl; }
+
+}
