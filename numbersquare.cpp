0c2e320f865ce4bfa999030b54ca0d5cd5b131f1
puneetcoder31 <puneetasp31@gmail.com>
2025-01-03 18:42:24 +0530

    to print number  pattern

---
+++ numbersquare.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){ int a;
+    cout<<("enter the number of rows you want=");
+    cin>>a;
+    int c;
+        cout<<("enter the number of column you want=");
+        cin>>c;
+    for(int x=1;x<=a;x++){
+        for(int b=1;b<=c;b++ ){
+        cout<<"  "<<b<<"  ";}
+        cout<<endl; }
+
+}
