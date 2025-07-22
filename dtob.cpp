c37d2a0e6917d9d235d30434efa61b1de0d6838a
puneetcoder31 <puneetasp31@gmail.com>
2025-07-22 19:20:11 +0530

    finally code

--- dtob.cpp
+++ dtob.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
 using namespace std;
-/*int  main(){
-    int num =0;
+
+int dtb(int deci){  // Function to convert decimal to binary
+    int binnum =0;
     int pow=1;
-    int deci;
-    cin>>deci;
        while(deci>0 ){
         int rem =deci%2;
         deci=deci/2;
-        num+=rem*pow;
+        binnum+=rem*pow;
         pow*=10; }
-cout<<"Decimal number to the binary  number is:"<<num;}*/
+        return binnum;}
+        
+        
+    int btd(int binn){ // Function to convert binary to decimal
+    int decnum =0;
+    int pow=1; 
+       while(binn>0 ){
+        int rem =binn%10;
+        binn=binn/10;
+        decnum+=rem*pow;
+        pow*=2; }
+        return decnum;}
 
-int main(){
-    int bin;
 
-    cin>>bin;
-    int ans=0;
-    int pow=1;
-    while(bin>0){
-        int rem=bin%10;
-        bin/=10;
-        ans+=rem*pow;
-        pow*=2;}
-    cout<<"Binary number to the decimal number is:"<<ans;
-    return 0;
+int  main(){
+   int choice;
+   cout<<"choose 1 for decimal to binary conversion\n";
+   cout<<"choose 2 for binary to decimal conversion\n";
+   cout<<"Enter your choice:";
+   cin>>choice;
 
-}
+   if (choice==1){
+      int dec;
+      cout<<"Enter the decimal number to convert to binary:\n";
+      cin>>dec;
+      
+      cout<<dtb(dec);}
+   else if (choice==2){
+         int bin;
+         cout<<"Enter the binary number to convert to decimal:";
+         cin>>bin;
+
+         cout<<btd(bin);}
+      else { cout<<"invalid choice";}
+      return 0;}
