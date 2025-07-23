a9d813a9fb479731d7e1bc804e76f08ceb3389d1
puneetcoder31 <puneetasp31@gmail.com>
2025-07-23 17:25:10 +0530

    code to find subarray

---
+++ subarry.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void subarray(int arr[],int s){
+    for(int start=0; start<s;start++){
+        for(int end =0;end<s;end++){
+            for (int i=start; i<=end;i++){
+                cout<<arr[i]<<" ";
+            }cout<<endl;
+        }
+    }        
+}
+int main (){
+    int s;
+    int arr[10];
+    cout<<"Size of the array you want="; cin>>s;
+    for ( int i=0;i<s;i++){
+        cout<<"enter the number the you want the array=";
+        int num;
+        cin>> arr[i];}
+        subarray(arr,s);
+    
+     return 0;}
+    
